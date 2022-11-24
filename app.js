867a564b13e98f7b726c1a9a54ce933d5dde0406
kartikey <dwivedi.k@northeastern.edu>
2022-11-24 14:06:25 -0500

    push updated code

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import HelloController from './controllers/hello-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import mongoose from "mongoose";
-mongoose.connect('mongodb://localhost:27017/tuiter');
-
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(express.json());
 app.use(cors());
